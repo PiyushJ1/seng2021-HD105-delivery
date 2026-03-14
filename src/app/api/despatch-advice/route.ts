afed7c05b76e01a80f474690c45d68726ad0472b
PiyushJ1 <piyxsh@gmail.com>
2026-03-15 01:46:35 +1100

    feat: add error validation for missing/invalid fields

--- src/app/api/despatch-advice/route.ts
+++ src/app/api/despatch-advice/route.ts
@@ -1,15 +1,22 @@
+import { DespatchAdviceRequest } from "@/src/types";
 import { NextResponse, NextRequest } from "next/server";
 
-export async function GET(req: NextRequest) {
-  const { searchParams } = req.nextUrl;
-  const despatchAdviceId = searchParams.get("despatchAdviceId");
+export async function POST(req: NextRequest) {
+  const body: DespatchAdviceRequest = await req.json();
 
-  // get specific despatch advice doc
-  if (despatchAdviceId) {
+  if (
+    typeof body.orderId !== "string" ||
+    typeof body.supplierPartyId !== "string" ||
+    typeof body.deliveryPartyId !== "string" ||
+    typeof body.despatchDate !== "string" ||
+    !Array.isArray(body.items) ||
+    body.items.length === 0
+  ) {
+    return NextResponse.json(
+      {
+        error: "Missing or invalid fields in the request body",
+      },
+      { status: 400 },
+    );
   }
-
-  // get all despatch advice docs
-  return NextResponse.json({ message: "this is the despatch endpoint" });
 }
-
-export async function POST(req: NextRequest) {}
