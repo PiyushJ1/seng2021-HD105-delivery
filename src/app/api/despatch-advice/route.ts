0f94cc3394ed6aec12820749bc0d8f50772c0abf
PiyushJ1 <piyxsh@gmail.com>
2026-03-15 14:13:21 +1100

    feat: create mock function for orderId + check for existing despatch doc

--- src/app/api/despatch-advice/route.ts
+++ src/app/api/despatch-advice/route.ts
@@ -1,9 +1,20 @@
 import { DespatchAdviceRequest } from "@/src/types";
 import { NextResponse, NextRequest } from "next/server";
+import clientPromise from "@/src/lib/mongodb";
+
+const client = await clientPromise;
+const db = client.db("myFirstDatabase");
+const collection = db.collection("despatch_advice");
+
+// mock fetching order for now
+async function getOrder(orderId: string) {
+  return orderId === "abc123" ? { orderId } : null;
+}
 
 export async function POST(req: NextRequest) {
   const body: DespatchAdviceRequest = await req.json();
 
+  // missing/invalid body validation
   if (
     typeof body.orderId !== "string" ||
     typeof body.supplierPartyId !== "string" ||
@@ -17,6 +28,22 @@ export async function POST(req: NextRequest) {
         error: "Missing or invalid fields in the request body",
       },
       { status: 400 },
+    );
+  }
+
+  // validate orderId exists
+  if (!getOrder(body.orderId)) {
+    return NextResponse.json(
+      { error: "the orderId was not found" },
+      { status: 404 },
+    );
+  }
+
+  const existingDoc = await collection.findOne({ orderId: body.orderId });
+  if (existingDoc) {
+    return NextResponse.json(
+      { error: "Despatch advice doc already exists for this order" },
+      { status: 409 },
     );
   }
 }
