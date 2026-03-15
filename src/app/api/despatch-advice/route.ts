9a08653cfa6b81a8f3b7e722b92ff7c6faf8e552
Piyush <157813396+PiyushJ1@users.noreply.github.com>
2026-03-15 16:43:20 +1100

    feat: implement POST/ desatch-advice route + mock db for test cases (#10)
    * feat: create custom data types
    
    * feat: add error validation for missing/invalid fields
    
    * feat: create mock function for orderId + check for existing despatch doc
    
    * feat: add validation to check if requested items have stock left
    
    * build: define config for loading env vars
    
    * fix(tests): update client requests + connect to db to emulate prod
    
    * feat: save despatch advice details to db + implement success response
    
    * fix(build): move db setup inside function handler

--- src/app/api/despatch-advice/route.ts
+++ src/app/api/despatch-advice/route.ts
@@ -1,15 +1,90 @@
 import { NextResponse, NextRequest } from "next/server";
+import { randomUUID } from "crypto";
+import { DespatchAdviceRequest } from "@/src/types";
+import clientPromise from "@/src/lib/mongodb";
 
-export async function GET(req: NextRequest) {
-  const { searchParams } = req.nextUrl;
-  const despatchAdviceId = searchParams.get("despatchAdviceId");
-
-  // get specific despatch advice doc
-  if (despatchAdviceId) {
-  }
-
-  // get all despatch advice docs
-  return NextResponse.json({ message: "this is the despatch endpoint" });
+// mock fetching order for now
+// TODO: fetch the actual order
+async function getOrder(orderId: string) {
+  return orderId === "abc123" ? { orderId } : null;
 }
 
-export async function POST(req: NextRequest) {}
+// mock getting the inventory for a product given its ID
+// TODO: fetch the actual inventory for a product
+async function getInventory(productId: string) {
+  return { productId, remainingQuantity: 100 };
+}
+
+export async function POST(req: NextRequest) {
+  // setup db connection
+  const client = await clientPromise;
+  const db = client.db(
+    process.env.NODE_ENV === "development" ? "test" : "production",
+  );
+  const collection = db.collection("despatch_advice");
+
+  const body: DespatchAdviceRequest = await req.json();
+
+  // missing/invalid body validation
+  if (
+    typeof body.orderId !== "string" ||
+    typeof body.supplierPartyId !== "string" ||
+    typeof body.deliveryPartyId !== "string" ||
+    typeof body.despatchDate !== "string" ||
+    !Array.isArray(body.items) ||
+    body.items.length === 0
+  ) {
+    return NextResponse.json(
+      {
+        error: "Missing or invalid fields in the request body",
+      },
+      { status: 400 },
+    );
+  }
+
+  // validate orderId exists
+  const order = await getOrder(body.orderId);
+  if (!order) {
+    return NextResponse.json(
+      { error: "the orderId was not found" },
+      { status: 404 },
+    );
+  }
+
+  const existingDoc = await collection.findOne({ orderId: body.orderId });
+  if (existingDoc) {
+    return NextResponse.json(
+      { error: "Despatch advice doc already exists for this order" },
+      { status: 409 },
+    );
+  }
+
+  // validate that item quantities requested have available stock
+  for (const item of body.items) {
+    const inventory = await getInventory(item.productId);
+    if (inventory.remainingQuantity < item.quantity) {
+      return NextResponse.json(
+        { error: "item quantity exceeds the quantity available" },
+        { status: 422 },
+      );
+    }
+  }
+
+  const despatchAdviceId = randomUUID();
+
+  // create and insert despatch advice "doc" into db
+  await collection.insertOne({
+    despatchAdviceId,
+    orderId: body.orderId,
+    supplierPartyId: body.supplierPartyId,
+    deliveryPartyId: body.deliveryPartyId,
+    despatchDate: body.despatchDate,
+    items: body.items,
+    status: "Complete",
+  });
+
+  return NextResponse.json({
+    despatchAdviceId,
+    status: "Complete",
+  });
+}
