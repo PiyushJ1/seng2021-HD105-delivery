98316d911d17a0cc8d7c12577489035e95cbd38a
PiyushJ1 <piyxsh@gmail.com>
2026-04-02 23:47:05 +1100

    feat: add additonal error checking for req body, role, and if user exists

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,6 +1,7 @@
 import clientPromise from "@/src/lib/mongodb";
 import { NextResponse, NextRequest } from "next/server";
 import validator from "validator";
+import { randomUUID } from "crypto";
 
 export async function POST(req: NextRequest) {
   const client = await clientPromise;
@@ -11,7 +12,7 @@ export async function POST(req: NextRequest) {
 
   const { email, password, role } = await req.json();
 
-	// check if email format is valid
+  // check if email format is valid
   if (!validator.isEmail(email)) {
     return NextResponse.json(
       { error: "Please enter a valid email address" },
@@ -19,5 +20,40 @@ export async function POST(req: NextRequest) {
     );
   }
 
-	
+  // missing/invalid body validation
+  if (typeof password !== "string" || typeof role !== "string") {
+    return NextResponse.json(
+      { error: "Password or role format is invalid" },
+      { status: 400 },
+    );
+  }
+
+  // ensure role is only either delivery or despatch
+  if (role !== "delivery" && role !== "despatch") {
+    return NextResponse.json(
+      {
+        error: "role can only be either 'delivery' or 'despatch'",
+      },
+      { status: 400 },
+    );
+  }
+
+  // check if the user already exists
+  const existingUser = await collection.findOne({ email: email });
+  if (existingUser) {
+    return NextResponse.json(
+      { error: "A user is already registered with this email" },
+      { status: 404 },
+    );
+  }
+
+  // register new user (save to db)
+  await collection.insertOne({
+    userId: randomUUID(),
+    email,
+    password,
+    role,
+  });
+
+  return NextResponse.json({ message: "success!" });
 }
