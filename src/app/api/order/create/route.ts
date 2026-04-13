b86966e21a8c0cbf9f7e93bc8c0a4cd92365bbb2
tashvi <tashvi.arora@student.unsw.edu.au>
2026-04-13 20:26:16 +1000

    fixing lint

--- src/app/api/order/create/route.ts
+++ src/app/api/order/create/route.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(req: NextRequest) {
   const client = await clientPromise;
   const db = client.db(
-    process.env.NODE_ENV === "development" ? "test" : "production"
+    process.env.NODE_ENV === "development" ? "test" : "production",
   );
   const users = db.collection("users");
 
@@ -22,28 +22,22 @@ export async function POST(req: NextRequest) {
 
   const body = await req.json();
 
-  if (
-    body.buyerEmail !== user.email &&
-    body.sellerEmail !== user.email
-  ) {
+  if (body.buyerEmail !== user.email && body.sellerEmail !== user.email) {
     return NextResponse.json(
       { error: "Your email must be buyer or seller" },
-      { status: 403 }
+      { status: 403 },
     );
   }
 
-  const res = await fetch(
-    `${process.env.API_BASE_URL}/v1/order/create`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${user.password}`,
-        "X-Party-Email": user.email,
-      },
-      body: JSON.stringify(body),
-    }
-  );
+  const res = await fetch(`${process.env.API_BASE_URL}/v1/order/create`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${user.password}`,
+      "X-Party-Email": user.email,
+    },
+    body: JSON.stringify(body),
+  });
 
   const data = await res.json();
 
